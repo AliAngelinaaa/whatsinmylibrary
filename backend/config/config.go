905f9409bdac68adf31ba13c35fcc76b49d98c64
aliangelinaaa <nanzum1140@gmail.com>
2026-09-25 12:06:33 -0400

    fixing up vercel file

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,10 +4,11 @@ import (
 	"backend/models"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/glebarez/sqlite"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -22,23 +23,56 @@ func InitEnv() {
 	log.Println("No .env file found, using environment variables")
 }
 
+func firstEnv(keys ...string) string {
+	for _, key := range keys {
+		if value := os.Getenv(key); value != "" {
+			return value
+		}
+	}
+	return ""
+}
+
+func databaseConfig() (driver string, dsn string) {
+	if url := firstEnv("DATABASE_URL", "POSTGRES_URL", "POSTGRES_PRISMA_URL"); url != "" {
+		return "postgres", url
+	}
+
+	driver = os.Getenv("DB_DRIVER")
+	dsn = os.Getenv("DB_DSN")
+	if dsn != "" && (driver == "postgres" || driver == "postgresql" ||
+		strings.HasPrefix(dsn, "postgres://") || strings.HasPrefix(dsn, "postgresql://")) {
+		return "postgres", dsn
+	}
+
+	// Demo on Vercel: ephemeral SQLite in /tmp. Data resets when the instance recycles.
+	if os.Getenv("VERCEL") != "" {
+		return "sqlite", "file:/tmp/whatsinmylibrary.db?_pragma=busy_timeout(5000)&_pragma=journal_mode(WAL)"
+	}
+
+	if dsn != "" {
+		if driver == "" {
+			driver = "sqlite"
+		}
+		return driver, dsn
+	}
+
+	if driver == "sqlite" {
+		return "sqlite", "whatsinmylibrary.db"
+	}
+	return "postgres", "host=localhost user=postgres password=postgres dbname=booksite port=5432 sslmode=disable"
+}
+
 func ConnectDatabase() {
-	driver := os.Getenv("DB_DRIVER")
-	dsn := os.Getenv("DB_DSN")
+	driver, dsn := databaseConfig()
 
 	var db *gorm.DB
 	var err error
 
 	switch driver {
 	case "sqlite":
-		if dsn == "" {
-			dsn = "whatsinmylibrary.db"
-		}
+		log.Println("Using SQLite at", dsn)
 		db, err = gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 	default:
-		if dsn == "" {
-			dsn = "host=localhost user=postgres password=postgres dbname=booksite port=5432 sslmode=disable"
-		}
 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	}
 
@@ -70,5 +104,13 @@ func ConnectDatabase() {
 }
 
 func DevAuthEnabled() bool {
-	return os.Getenv("DEV_AUTH") == "true"
+	switch strings.ToLower(os.Getenv("DEV_AUTH")) {
+	case "true", "1", "yes":
+		return true
+	case "false", "0", "no":
+		return false
+	default:
+		// Demo login works on Vercel without extra env vars.
+		return os.Getenv("VERCEL") != ""
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,18 +3,21 @@ module backend
 go 1.23.0
 
 require (
+	github.com/glebarez/sqlite v1.11.0
 	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/joho/godotenv v1.5.1
 	github.com/microcosm-cc/bluemonday v1.0.27
 	golang.org/x/oauth2 v0.28.0
 	gorm.io/driver/postgres v1.5.11
-	gorm.io/driver/sqlite v1.5.7
 	gorm.io/gorm v1.25.12
 )
 
 require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -22,9 +25,15 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.23.1 // indirect
 )
